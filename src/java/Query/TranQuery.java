75a3b95a56caae9d987447398637e09351d9d1e2
Benaya <107478721+benrazor45@users.noreply.github.com>
2022-07-12 14:41:51 +0700

    Update TranQuery.java

--- src/java/Query/TranQuery.java
+++ src/java/Query/TranQuery.java
@@ -1,14 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Query;
+package BooksQuery;
 
 /**
  *
- * @author User
+ * @author Benaya
  */
 public class TranQuery {
     public String gets = "SELECT * FROM transaction";
+    public String creates = "INSERT INTO transaction (Books_ID, Borrower_Name, " + 
+            "Borrow_Date, Return_Date) VALUES (?, ?, ?, ?)";
 }
