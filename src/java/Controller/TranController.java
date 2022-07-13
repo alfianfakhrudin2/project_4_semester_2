cb5551df569de5712c81bdf338a6cf65b6d5b106
Benaya <107478721+benrazor45@users.noreply.github.com>
2022-07-13 11:53:13 +0700

    Update TranController.java

--- src/java/Controller/TranController.java
+++ src/java/Controller/TranController.java
@@ -1,24 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Controller;
-import java.sql.ResultSet;
-import Query.TranQuery;
-/**
- *
- * @author User
- */
-public class TranController extends BaseController {
-      
-    TranQuery querytran = new TranQuery();
-    public ResultSet gets() {
-        String sql = this.querytran.gets;
-        return this.get(sql);
-    }
-   
-}
+
 package Controller;
 
 import java.sql.ResultSet;
